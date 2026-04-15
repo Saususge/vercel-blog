5dd3deeeb064fd8ecdf2dc6c972dd942ebdf5289
Hozo <epiklife@naver.com>
2026-04-15 23:01:42 +0900

    fixed error

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
+import mermaid from "astro-mermaid";
 import {
   transformerNotationDiff,
   transformerNotationHighlight,
